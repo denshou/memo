6cfcf360fde87d925cbe6df6e9b46ed09cd874d6
denshou <132555762+denshou@users.noreply.github.com>
2023-08-07 19:21:14 +0900

    memo+detaspace

--- main.py
+++ main.py
@@ -21,4 +21,20 @@ def create_memo(memo:Memo):
 def read_Memo():
     return memos
 
+@app.put("/memos/{memo_id}")
+def put_memo(req_memo:Memo): # request body   m : 기존 내용
+    for memo in memos: # memos 배열을 돌면서
+        if memo.id == req_memo.id: 
+            memo.content = req_memo.content
+            return "수정했습니다."
+    return "실패"    
+
+@app.delete("/memos/{memo_id}") #memo_id : 전달받은 삭제할 메모의 id
+def put_memo(memo_id): 
+    for index, memo in enumerate(memos): # for 문을 사용할 때 index와 값을 같이 쓰려면 enumerate() 사용
+        if memo.id == memo_id: 
+            memos.pop(index)
+            return "성공했습니다."
+    return "실패"   
+
 app.mount("/",StaticFiles(directory='static', html=True),name='static')
--- static/app.js
+++ static/app.js
@@ -1,8 +1,43 @@
+async function editMemo(event) {
+  const id = event.target.dataset.id;
+  const editInput = prompt("수정할 내용을 입력해주세요");
+  const res = await fetch(`/memos/${id}`, {
+    method: "PUT", // PUT : 내용을 수정할 때 사용
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id,
+      content: editInput,
+    }),
+  });
+  readMemo();
+}
+
+async function deleteMemo(event) {
+  const id = event.target.dataset.id;
+  const res = await fetch(`/memos/${id}`, {
+    method: "DELETE",
+  });
+  readMemo();
+}
+
 function displayMemo(memo) {
   const ul = document.querySelector("#memo-ul");
   const li = document.createElement("li");
+  const editBtn = document.createElement("button");
+  const delBtn = document.createElement("button");
+  editBtn.innerText = "수정";
+  editBtn.addEventListener("click", editMemo);
+  editBtn.dataset.id = memo.id;
+  delBtn.innerText = "삭제";
+  delBtn.addEventListener("click", deleteMemo);
+  delBtn.dataset.id = memo.id;
+
   li.innerText = `[id:${memo.id}] ${memo.content}`;
   ul.appendChild(li);
+  li.appendChild(editBtn);
+  li.appendChild(delBtn);
 }
 
 async function readMemo() {
